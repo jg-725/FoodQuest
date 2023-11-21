8dd960a8311f0d0778c09c8b369ba23785995d1c
Hman00 <98668690+Hman00@users.noreply.github.com>
2023-11-20 20:12:45 -0500

    Update testregister.php

--- Database/testregister.php
+++ Database/testregister.php
@@ -1,10 +1,5 @@
-
-
-
-
-
-
 <?php
+
 
 // Connect to the database
 $servername = "localhost";
@@ -19,59 +14,36 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Function to hash password
-function hashPassword($password) {
-    return password_hash($password, PASSWORD_DEFAULT);
-}
-
 // Assume that you have obtained $username, $password, $email, $firstname, $lastname from the form submission
 
 // Check if the user already exists in the database
-$sql_check = "SELECT * FROM users WHERE username = ? OR email = ?";
-$stmt_check = mysqli_prepare($conn, $sql_check);
-mysqli_stmt_bind_param($stmt_check, "ss", $username, $email);
-mysqli_stmt_execute($stmt_check);
-$result_check = mysqli_stmt_get_result($stmt_check);
+$sql_check = "SELECT * FROM User WHERE username = '$username' OR email = '$email'";
+$result = mysqli_query($conn, $sql_check);
 
-if (mysqli_num_rows($result_check) > 0) {
+if (mysqli_num_rows($result) > 0) {
     // User already exists
     echo "User already exists in the database.\n";
     $userExists = true;
 } else {
     // User does not exist
-    // Hash the password before storing
-    $hashedPassword = hashPassword($password);
+    // Insert the user data into the database
+    $sql = "INSERT INTO User (username, password, email, firstname, lastname) VALUES ('$username', '$password', '$email', '$firstname', '$lastname')";
 
-    // Insert the user data into the database using a prepared statement
-    $sql_insert = "INSERT INTO users (username, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)";
-    $stmt_insert = mysqli_prepare($conn, $sql_insert);
-    mysqli_stmt_bind_param($stmt_insert, "sssss", $username, $hashedPassword, $email, $firstname, $lastname);
-
-    if (mysqli_stmt_execute($stmt_insert)) {
+    if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
         $userExists = false;
 
-        // Insert additional user information into another table
-        $sql_insert_additional = "INSERT INTO user_additional_info (username, additional_info_column) VALUES (?, ?)";
-        $stmt_insert_additional = mysqli_prepare($conn, $sql_insert_additional);
-        $additionalInfo = "Some additional information"; // Replace with the actual data
-        mysqli_stmt_bind_param($stmt_insert_additional, "ss", $username, $additionalInfo);
-        
-        if (mysqli_stmt_execute($stmt_insert_additional)) {
+        // Insert additional user information into the User table
+        $sql_insert_additional = "INSERT INTO User (username, additional_info_column) VALUES ('$username', 'Some additional information')";
+        if (mysqli_query($conn, $sql_insert_additional)) {
             echo "Additional user information inserted successfully";
         } else {
-            echo "Error: " . $sql_insert_additional . "<br>" . mysqli_stmt_error($stmt_insert_additional);
+            echo "Error: " . $sql_insert_additional . "<br>" . mysqli_error($conn);
         }
     } else {
-        echo "Error: " . $sql_insert . "<br>" . mysqli_stmt_error($stmt_insert);
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 }
 
-// Close the prepared statements
-mysqli_stmt_close($stmt_check);
-mysqli_stmt_close($stmt_insert);
-mysqli_stmt_close($stmt_insert_additional);
-
 // Close the database connection
 mysqli_close($conn);
-
