d7e772585f4e976d1900cd5e20ce8047664c07f2
Hman00 <98668690+Hman00@users.noreply.github.com>
2023-11-20 20:21:13 -0500

    Update testregister.php

--- Database/testregister.php
+++ Database/testregister.php
@@ -1,6 +1,6 @@
+
+
 <?php
-
-
 // Connect to the database
 $servername = "localhost";
 $username_db = "test";
@@ -14,10 +14,8 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Assume that you have obtained $username, $password, $email, $firstname, $lastname from the form submission
-
 // Check if the user already exists in the database
-$sql_check = "SELECT * FROM User WHERE username = '$username' OR email = '$email'";
+$sql_check = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
 $result = mysqli_query($conn, $sql_check);
 
 if (mysqli_num_rows($result) > 0) {
@@ -27,23 +25,31 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     // User does not exist
     // Insert the user data into the database
-    $sql = "INSERT INTO User (username, password, email, firstname, lastname) VALUES ('$username', '$password', '$email', '$firstname', '$lastname')";
+    $sql = "INSERT INTO users (username, password, email, firstname, lastname) VALUES ('$username', '$password', '$email', '$firstname', '$lastname')";
 
     if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
         $userExists = false;
-
-        // Insert additional user information into the User table
-        $sql_insert_additional = "INSERT INTO User (username, additional_info_column) VALUES ('$username', 'Some additional information')";
-        if (mysqli_query($conn, $sql_insert_additional)) {
-            echo "Additional user information inserted successfully";
-        } else {
-            echo "Error: " . $sql_insert_additional . "<br>" . mysqli_error($conn);
-        }
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 }
 
+// Function to send a message
+function send_msg($sender, $message)
+{
+    global $conn;
+    if (!empty($sender) && !empty($message)) {
+        $sender = mysqli_real_escape_string($conn, $sender);
+        $message = mysqli_real_escape_string($conn, $message);
+        $query = "INSERT INTO chat VALUES(null, '$sender', '$message')";
+        $run = mysqli_query($conn, $query);
+        return $run ? true : false;
+    } else {
+        return false;
+    }
+}
+
 // Close the database connection
 mysqli_close($conn);
+?>
